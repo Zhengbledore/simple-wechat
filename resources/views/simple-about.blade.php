5d425e2a22b15bbfffbc390e5775ade4314a522f
vagrant <vagrant@homestead>
2017-08-23 14:44:19 +0000

    add share js

--- resources/views/simple-about.blade.php
+++ resources/views/simple-about.blade.php
@@ -961,5 +961,34 @@
     'onMenuShareTimeline',
     'onMenuShareAppMessage'
     ), true) !!} );
+
+    wx.ready(function(){
+        wx.onMenuShareTimeline({
+            title: '测试123', // 分享标题
+            link: 'http://magical.echofault.com/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: '', // 分享图标
+            success: function () {
+                // 用户确认分享后执行的回调函数
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+
+        wx.onMenuShareAppMessage({
+            title: '测试321', // 分享标题
+            desc: '啦啦啦啦啦', // 分享描述
+            link: 'http://magical.echofault.com/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: '', // 分享图标
+            type: 'link', // 分享类型,music、video或link，不填默认为link
+            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () {
+                // 用户确认分享后执行的回调函数
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+    });
 </script>
 </html>
