747922b8bad589306143f26b929ada984cd42d33
郑堃铭 <18806639898@139.com>
2017-08-24 10:15:47 +0800

    add api

--- resources/views/simple-about.blade.php
+++ resources/views/simple-about.blade.php
@@ -955,7 +955,7 @@
 </script>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript" charset="utf-8">
-    wx.config( <?php echo $js->config(array('onMenuShareQQ', 'onMenuShareWeibo'), false) ?>);
+    wx.config( <?php echo $js->config(array('onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareTimeline', 'onMenuShareAppMessage'), false) ?>);
 
     wx.ready(function(){
         wx.onMenuShareTimeline({
