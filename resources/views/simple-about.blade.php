e90ee293ccd234de503444e642b7487437851be0
郑堃铭 <18806639898@139.com>
2017-08-24 10:14:29 +0800

    close debug jssdk

--- resources/views/simple-about.blade.php
+++ resources/views/simple-about.blade.php
@@ -955,7 +955,7 @@
 </script>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript" charset="utf-8">
-    wx.config( <?php echo $js->config(array('onMenuShareQQ', 'onMenuShareWeibo'), true) ?>);
+    wx.config( <?php echo $js->config(array('onMenuShareQQ', 'onMenuShareWeibo'), false) ?>);
 
     wx.ready(function(){
         wx.onMenuShareTimeline({
